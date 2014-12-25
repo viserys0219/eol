96a4c9dcd0af42fd4aaffb11ec87d60250cbfa33
viserys <viserys@outlook.com>
2014-12-25 16:18:56 +0800

    Pass the visit FT

--- eol/urls.py
+++ eol/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
+    url(r'^$', 'modeling.views.home_page', name="home"),
     # Examples:
     # url(r'^$', 'eol.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
--- modeling/views.py
+++ modeling/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.http import HttpResponse
+def home_page(request):
+    return HttpResponse("<html><title>EOL homepage</title></html>")
