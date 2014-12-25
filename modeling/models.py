b14224deea227196f572dddad9ac9dbbdc78476d
viserys <viserys@outlook.com>
2014-12-25 16:50:57 +0800

    Before makemigrations

--- modeling/models.py
+++ modeling/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Creature(models.Model):
+    c_id = models.TextField(default="")
+    s_name = models.TextField(default="")
+    c_name = models.TextField(default="")
+    category = models.TextField(default="")
+    iucn_level = models.TextField(default="")
+    level = models.TextField(default="")
+    location = models.TextField(default="")
+    location_number = models.TextField(default="")
+    location_style = models.TextField(default="")
+    
+    
--- modeling/tests.py
+++ modeling/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from modeling.models import Creature
 
-# Create your tests here.
+class CreatrueModelTest(TestCase):
+    def test_retrieving_items(self):
+        #Creature.objects.create(c_id="355488")
+        c = Creature()
+        c.c_id = "355488"
+        c.save()
+        first_item = Creature.objects.first()
+        self.assertEqual(first_item.c_id, "355488")
